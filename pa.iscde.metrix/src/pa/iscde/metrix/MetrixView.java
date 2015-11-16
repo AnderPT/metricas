57c4a8dca1bbb684553664320ab7a01f9ae36477
Andre Carvalho <Andre Carvalho@172.17.7.239>
2015-11-16 18:38:08 +0000

    test commit

--- pa.iscde.metrix/src/pa/iscde/metrix/MetrixView.java
+++ pa.iscde.metrix/src/pa/iscde/metrix/MetrixView.java
@@ -10,6 +10,7 @@ import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
@@ -26,11 +27,10 @@ public class MetrixView implements PidescoView {
 	@Override
 	public void createContents(Composite viewArea, Map<String, Image> imageMap) {
 
-        
         Table table = new Table(viewArea, SWT.MULTI | SWT.BORDER | SWT.FULL_SELECTION);
         table.setLinesVisible(true);
         table.setHeaderVisible(true);
-
+ 
         String[] titles = { "Metric Name", "Value"};
         for (int i = 0; i < titles.length; i++) {
             TableColumn column = new TableColumn(table, SWT.NONE);
